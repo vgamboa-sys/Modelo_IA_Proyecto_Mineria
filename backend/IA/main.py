97a9299de2c12989480673f1d2c0a47c2f1ff15c
IgnaBascu1 <ibascunan@acl.cl>
2025-10-24 16:15:45 -0300

    prompt con json integrado

--- backend/IA/main.py
+++ backend/IA/main.py
@@ -18,11 +18,11 @@ else:
 cliente_ppj = genai.Client(api_key=api_key)
 
 # Cargar JSON
-with open("datos_clima.json", "r", encoding="utf-8") as f:
+with open("backend\IA\datos_clima.json", "r", encoding="utf-8") as f:
     datos = json.load(f)
 
 # Generación Prompt
-categorias_ejemplo =  [
+categorias_ejemplo = [
 "alerta_temperatura", 
 "alerta_visibilidad", 
 "alerta_viento", 
@@ -30,26 +30,24 @@ categorias_ejemplo =  [
 "alerta_calidad_aire",
 "otro"]
 
-# prompt = "Dame el pokemon mas fuerte con su descripción. Max. 30 palabras"
-# prompt = f"En base a estos datos del json {json.dumps(datos, ensure_ascii=False, separators=(",", ":"))}, dime algunas recomendaciones de seguridad. Max 50 palabras"
-prompt = """Eres un sistema de monitoreo minero. A partir de los siguientes datos de la mina "Mina Los Bronces", dame recomendaciones de seguridad breves (máx. 50 palabras). Devuelve solo texto:
+prompt = f"""Eres un sistema de monitoreo minero. A partir de los siguientes datos de la mina "{datos['ciudad']}", dame recomendaciones de seguridad breves (máx. 50 palabras). Devuelve solo texto:
 
-Ciudad: Mina Los Bronces
-Temperatura (°C): -5.12
-Humedad (%): 59
-Visibilidad (mts): 6967
-Nubes (%): 100
-Viento velocidad (m/s): 4.08
-Viento ráfaga (m/s): 7.02
-Lluvia 1h (mm): 0
-Nieve 1h (mm): 0.13
-Polución PM10: 12.27
-Polución PM2.5: 9.32
-CO: 155.28
-SO2: 1
-NO2: 2.56
-O3: 54.16
-NH3: 3.51
+Ciudad: {datos['ciudad']}
+Temperatura (°C): {datos['temperatura_c']}
+Humedad (%): {datos['humedad_pct']}
+Visibilidad (mts): {datos['visibilidad_mts']}
+Nubes (%): {datos['nubes_pct']}
+Viento velocidad (m/s): {datos['viento_velocidad_ms']}
+Viento ráfaga (m/s): {datos['viento_rafaga_ms']}
+Lluvia 1h (mm): {datos['lluvia_1h_mm']}
+Nieve 1h (mm): {datos['nieve_1h_mm']}
+Polución PM10: {datos['polucion_pm10']}
+Polución PM2.5: {datos['polucion_pm2_5']}
+CO: {datos['polucion_co']}
+SO2: {datos['polucion_so2']}
+NO2: {datos['polucion_no2']}
+O3: {datos['polucion_o3']}
+NH3: {datos['polucion_nh3']}
 """
 
 
@@ -73,9 +71,7 @@ try:
     if not texto_genai:
         raise ValueError("El modelo devolvió una respuesta vacía o no válida.")
     
-    print("Respuesta cruda del modelo:\n", texto_genai)
-    
-    # print("Respuesta JSON:", json.dumps(resultado, indent=2, ensure_ascii=False))
+    print("Respuesta cruda del modelo:\n", texto_genai)    
     
 except Exception as e:
     print("Error al llamar a Gemini:", e)
