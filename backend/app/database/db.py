560e85af4541ecdd8cb0643b0a46615a92db8b43
IgnaBascu <igna.bascu@gmail.com>
2025-10-30 15:34:16 -0300

    configuracion db

--- backend/app/database/db.py
+++ backend/app/database/db.py
@@ -3,6 +3,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 DB_URL = os.getenv("DATABASE_URL", "sqlite:///database/app.db")
-engine = create_engine(DB_URL, connect_args={"check_same_thread": False})
+
+connect_args = {}
+if DB_URL.startswith("sqlite"):
+    connect_args = {"check_same_thread": False}
+
+engine = create_engine(DB_URL, connect_args=connect_args)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
